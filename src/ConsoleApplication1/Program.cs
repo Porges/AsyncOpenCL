efe92c2fc2584155ade94b7221ba56ddb9553c33
George Pollard <porges@porg.es>
2013-02-20 23:45:25 +1300

    a more interesting sample program

--- src/ConsoleApplication1/Program.cs
+++ src/ConsoleApplication1/Program.cs
@@ -28,35 +28,46 @@ namespace ConsoleApplication1
             int maxBytes = 10000;
             var device = Platform.GetPlatforms().First().GetDevices(DeviceType.Gpu).First();
             using (var ctx = new Context(device))
-            using (var buffer = new Buffer(ctx, (ulong)maxBytes))
             using (var queue = new CommandQueue(ctx, device))
             {
+                var tasks = new List<Task>();
                 for (int bytes = 1; bytes <= maxBytes; ++bytes)
-                {
-                    await RunCopies(bytes, queue, buffer);
-                }
+                    tasks.Add(RunCopies(bytes, queue, ctx));
+
+                // nothing runs until...
+                queue.Flush(); // pow!
+
+                // now wait for them all to finish
+                await Task.WhenAll(tasks);
             }
         }
 
-        private static async Task RunCopies(int bytes, CommandQueue queue, Buffer buffer)
+        private static async Task RunCopies(int bytes, CommandQueue queue, Context ctx)
         {
-            var src = new byte[bytes];
-            var dst = new byte[bytes];
-
-            for (int i = 0; i < src.Length; ++i)
-                src[i] = (byte) i;
-
-            using (var writeEvent = queue.EnqueueWrite(buffer, src))
-            using (var readEvent = queue.EnqueueRead(buffer, dst, after: writeEvent))
+            using (var buffer = new Buffer(ctx, (ulong) bytes))
             {
-                queue.Flush();
-                await readEvent;
+                var src = new byte[bytes];
+                var dst = new byte[bytes];
 
-                if (!src.SequenceEqual(dst))
+                for (int i = 0; i < src.Length; ++i)
+                    src[i] = (byte) i;
+
+                // queue a write to the buffer
+                using (var writeEvent = queue.EnqueueWrite(buffer, src))
+
+                // queue a read to happen after the write completes
+                using (var readEvent = queue.EnqueueRead(buffer, dst, after: writeEvent))
                 {
-                    Console.WriteLine("Error! Not equal:");
-                    Console.WriteLine(string.Join(",", src));
-                    Console.WriteLine(string.Join(",", dst));
+                    // wait for the read to complete before continuing
+                    await readEvent;
+
+                    // if we don't await then `dst` might not be populated
+                    if (!src.SequenceEqual(dst))
+                    {
+                        Console.WriteLine("Error! Not equal:");
+                        Console.WriteLine(string.Join(",", src));
+                        Console.WriteLine(string.Join(",", dst));
+                    }
                 }
             }
         }
